8ff67d398e0ae5ca7d7427423ef75f3183d43d2c
Erik Nordby <nordby.erik3@gmail.com>
2021-02-19 21:15:13 -0500

    made it actually run

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from datetime import datetime
 
 app = flask.Flask(__name__)
 
-@approute('/')
+@app.route('/')
 def base_page():
     return flask.render_template("base.html", page_title="Test")
+
+@app.route("/info")
+def info_page():
+    return flask.render_template("info.html", page_title="Information")
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
