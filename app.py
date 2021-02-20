e719db75bb06691edca0e427337ccf1f061452d4
the-jackson-perry <jdp99@pitt.edu>
2021-02-19 22:33:14 -0500

    display names of user's friends on friend page

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 app = flask.Flask(__name__)
 
-default_user = {'name':'default_user', 'bio':'default_bio', 'color-scheme':'default'}
+default_user = {'name':'default_user', 'bio':'default_bio', 'color-scheme':'default', 'friends':['friend1', 'friend2', 'friend3']}
 
-@app.route('/')
+@app.route('/base')
 def base_page():
     return flask.render_template("base.html", user=default_user, page_title="Test")
 
@@ -19,9 +19,14 @@ def info_page():
 def profile():
     return flask.render_template("profile.html", user=default_user, page_title="Profile")
 
+@app.route("/")
 @app.route("/dashboard")
 def dashboard():
     return flask.render_template("dashboard.html", user=default_user, page_title="Dashboard")
+
+@app.route("/friends")
+def dashboard():
+    return flask.render_template("friends.html", user=default_user, page_title="Friends")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
