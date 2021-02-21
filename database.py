63086668179181bef45a2de57087932960e24e3b
Erik Nordby <nordby.erik3@gmail.com>
2021-02-21 01:56:43 -0500

    Graph stuff

--- database.py
+++ database.py
@@ -2,17 +2,12 @@ from google.cloud import datastore
 
 datastore_client = datastore.Client.from_service_account_json('service-account.json')
 
-def save(entity):
-    datastore_client.put(entity)
-
-
 def add_post(screen_name, message, time):
-    
     post = {'screen_name': screen_name, 'message': message, 'time': time}
     msg_board = get_board()
     msg_board['message_list'].append(post)
     print(msg_board)
-    save(msg_board)
+    datastore_client.put(msg_board)
 
 
 def get_board():
@@ -23,6 +18,7 @@ def get_board():
         board['message_list']
     except:
         board['message_list'] = [{'screen_name': 'Filos Team', 'message': 'Welcome to Filos!', 'time': '2:20pm 2020-02-20'}]
+
     print(board)
     return board
 
--- static/javascript/info.js
+++ static/javascript/info.js
@@ -56,36 +56,10 @@ national_depression = d3.csv("../static/dataset/national_depression.csv")
 
 national_depression.then( 
     data => {
-            console.log(data);
             render_line_graph(data, svg);
     }
 );
 
-
-svg = d3.select("#map")
-    .append("svg")
-    .attr("viewBox", [0, 0, 975, 610]);
-
-var width = 960;
-var height = 500;
-
-var lowColor = '#f9f9f9'
-var highColor = '#bc2a66'
-
-    // D3 Projection
-var projection = d3.geoAlbersUsa()
-    .translate([width / 2, height / 2]) // translate to center of screen
-    .scale([1000]); // scale things down so see entire US
-
-    // Define path generator
-var path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
-    .projection(projection); // tell path generator to use albersUsa projection
-
-    //Create SVG element and append map to the SVG
-var svg = d3.select("map")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
 
 const render_map = filename => {
     //Width and height of map
@@ -100,110 +74,113 @@ const render_map = filename => {
     .translate([width / 2, height / 2]) // translate to center of screen
     .scale([1000]); // scale things down so see entire US
 
+
     // Define path generator
     var path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
     .projection(projection); // tell path generator to use albersUsa projection
 
     //Create SVG element and append map to the SVG
-    var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var svg = d3.select("#map")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
     var dataArray = [];
+    var dataArray2 = [];
     // Load in my states data!
     d3.csv(filename, function(data) {
-        console.log(data[d])
-        dataArray = dataArray.push(parseFloat(data.Value))
+        dataArray.push( {"Value":parseFloat(data.Value), "State": data.State })
+        dataArray2.push( parseFloat(data.Value) );
     });
 
-    var minVal = d3.min(dataArray)
-    var maxVal = d3.max(dataArray)
-    var ramp = d3.scaleLinear().domain([minVal,maxVal]).range([lowColor,highColor])
+    console.log(dataArray2)
+    var minVal = 17
+    var maxVal = 38
     
-    console.log("Data Array")
-    console.log(dataArray)
+    var ramp = d3.scaleLinear().domain([minVal,maxVal]).range([lowColor,highColor])
+
     // Load GeoJSON data and merge with states data
-    d3.json("../static/dataset/us.json", function(json) {
-
+    d3.json("../static/dataset/us.json").then( f = json => {
+        
         // Loop through each state data value in the .csv file
-        for (var i = 0; i < data.length; i++) {
+        for (var i = 0; i < dataArray.length; i++) {
+            // Grab State Name
+            var dataState = dataArray[i].State;
+            // Grab data value 
+            var dataValue = dataArray[i].Value;
 
-        // Grab State Name
-        var dataState = data[i].State;
+            // Find the corresponding state inside the GeoJSON
+            for (var j = 0; j < json.features.length; j++) {
+                var jsonState = json.features[j].properties.name;
+                if (dataState == jsonState) {
+                    // Copy the data value into the JSON
+                    json.features[j].properties.Value = dataValue;
 
-        // Grab data value 
-        var dataValue = data[i].Value;
-
-        // Find the corresponding state inside the GeoJSON
-        for (var j = 0; j < json.features.length; j++) {
-            var jsonState = json.features[j].properties.name;
-
-            if (dataState == jsonState) {
-
-            // Copy the data value into the JSON
-            json.features[j].properties.value = dataValue;
-
-            // Stop looking through the JSON
-            break;
+                    // Stop looking through the JSON
+                    break;
+                }
             }
         }
-        }
+        return json;
+    }).then( f = json => {
 
-        // Bind the data to the SVG and create one path per GeoJSON feature
-        svg.selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("stroke", "#fff")
-        .style("stroke-width", "1")
-        .style("fill", function(d) { return ramp(d.properties.value) });
-        
-            // add a legend
-            var w = 140, h = 300;
+            console.log(json.features);
+                // Bind the data to the SVG and create one path per GeoJSON feature
+            svg.selectAll("path")
+                .data(json.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("stroke", "#fff")
+                .style("stroke-width", "1")
+                .style("fill", function(d) { 
+                    return ramp(d.properties.Value) 
+                });
+                    // add a legend
+                    var w = 140, h = 300;
 
-            var key = d3.select("body")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h)
-                .attr("class", "legend");
+                    var key = d3.select("#map")
+                        .append("svg")
+                        .attr("width", w)
+                        .attr("height", h)
+                        .attr("class", "legend");
 
-            var legend = key.append("defs")
-                .append("svg:linearGradient")
-                .attr("id", "gradient")
-                .attr("x1", "100%")
-                .attr("y1", "0%")
-                .attr("x2", "100%")
-                .attr("y2", "100%")
-                .attr("spreadMethod", "pad");
+                    var legend = key.append("defs")
+                        .append("svg:linearGradient")
+                        .attr("id", "gradient")
+                        .attr("x1", "100%")
+                        .attr("y1", "0%")
+                        .attr("x2", "100%")
+                        .attr("y2", "100%")
+                        .attr("spreadMethod", "pad");
 
-            legend.append("stop")
-                .attr("offset", "0%")
-                .attr("stop-color", highColor)
-                .attr("stop-opacity", 1);
-                
-            legend.append("stop")
-                .attr("offset", "100%")
-                .attr("stop-color", lowColor)
-                .attr("stop-opacity", 1);
+                    legend.append("stop")
+                        .attr("offset", "0%")
+                        .attr("stop-color", highColor)
+                        .attr("stop-opacity", 1);
+                        
+                    legend.append("stop")
+                        .attr("offset", "100%")
+                        .attr("stop-color", lowColor)
+                        .attr("stop-opacity", 1);
 
-            key.append("rect")
-                .attr("width", w - 100)
-                .attr("height", h)
-                .style("fill", "url(#gradient)")
-                .attr("transform", "translate(0,10)");
+                    key.append("rect")
+                        .attr("width", w - 100)
+                        .attr("height", h)
+                        .style("fill", "url(#gradient)")
+                        .attr("transform", "translate(0,10)");
 
-            var y = d3.scaleLinear()
-                .range([h, 0])
-                .domain([minVal, maxVal]);
+                    var y = d3.scaleLinear()
+                        .range([h, 0])
+                        .domain([minVal, maxVal]);
 
-            var yAxis = d3.axisRight(y);
+                    var yAxis = d3.axisRight(y);
 
-            key.append("g")
-                .attr("class", "y axis")
-                .attr("transform", "translate(41,10)")
-                .call(yAxis)
-            });
+                    key.append("g")
+                        .attr("class", "y axis")
+                        .attr("transform", "translate(41,10)")
+                        .call(yAxis)
+    });
 }
 
 render_map("../static/dataset/national_depression_by_state_1.csv")
+render_map("../static/dataset/national_depression_by_state_12.csv")
